0561dc9064fe503de852ff6186f10e3f892ea335
elachapelle-pointer <eric@pointercreative.com>
2025-09-04 20:10:35 -0400

    colors, fonts

--- src/views/ColorsView.vue
+++ src/views/ColorsView.vue
@@ -13,27 +13,18 @@
       <div class="color" style="background: #580000;">
         <span style="color: #F7E6CA;">#580000</span>
       </div>
+      <div class="color" style="background: #904134;">
+        <span style="color: #F7E6CA;">#904134</span>
+      </div>
       <div class="color" style="background: #BC8410;">
         <span style="color: #FFFFFF;">#BC8410</span>
       </div>
       <div class="color" style="background: #F7E6CA;">
         <span style="color: #101217;">#F7E6CA</span>
       </div>
-      <!-- <div class="color" style="background: #8f0000;">
-        <span>#8f0000</span>
+      <div class="color" style="background: #ECE2DD;">
+        <span style="color: #101217;">#ECE2DD</span>
       </div>
-      <div class="color" style="background: #011638;">
-        <span>#011638</span>
-      </div>
-      <div class="color" style="background: #933b2b;">
-        <span>#933b2b</span>
-      </div>
-      <div class="color" style="background: #c8b38b;">
-        <span>#c8b38b</span>
-      </div>
-      <div class="color" style="background: #f1d0b8;">
-        <span>#f1d0b8</span>
-      </div> -->
     </div>
   
   </div>
